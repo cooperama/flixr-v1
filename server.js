d037d7c3105f1ae9daaae1cce25386424fd1c4f3
cooperama <coope133@gmail.com>
2020-10-09 15:17:56 -0700

    Adds server requirements

---
+++ server.js
@@ -0,0 +1,16 @@
+// ---------------------- Server Requirements
+require('dotenv').config();
+const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const routes = require('./routes');
+var db = require ('./models');
+
+
