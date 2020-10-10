63b3e763555f5b8c77f88996999ab4018b93472b
cooperama <coope133@gmail.com>
2020-10-09 17:30:56 -0700

    Adds partial header and footer files and sets home and 404 routes

--- server.js
+++ server.js
@@ -51,9 +51,12 @@ app.use(methodOverride('_method'));
 // ---------------------- Routes 
 
 app.get('/', (req, res) => {
-  res.send('<h1>Movie DB</h1>')
-})
+  res.render('index');
+});
 
+app.get('*', (req, res) => {
+  res.render('404');
+});
 
 // ---------------------- Listener
 app.listen(PORT, () => {
