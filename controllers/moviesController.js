1bed0e67516e1ac389c0630ebdda93ebdae09db8
cooperama <coope133@gmail.com>
2020-10-12 15:33:10 -0700

    Adds more back to front end functionality and adds to views

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -14,7 +14,7 @@ router.get('/recommendations', async (req, res) => {
             "vote_average.gte": req.query.vote_average_gte,
             "release_date.gte": req.query.release_date_gte,
             "with_runtime.gte": req.query.with_runtime_gte,
-            "vote_count.gte": 100,
+            "vote_count.gte": req.query.vote_count_gte,
         }
     })
 
@@ -23,7 +23,7 @@ router.get('/recommendations', async (req, res) => {
         imageEndpoint: 'https://image.tmdb.org/t/p/w500'
     }
 
-    res.render('/movies/recommendations', context);
+    res.render('movies/recommendations', context);
 
     console.log(response.data);
     return res.status(200).json(response.data)
@@ -33,7 +33,20 @@ router.get('/recommendations', async (req, res) => {
     }
 })
 
+// router.get('/recommendations', async (req, res) => {
+//     try {
+//         let imageResponse = await axios.get('https://image.tmdb.org/t/p/w500', {
+//             params: {
+//                 api_key: '64bbb4feb014546a2feb336e5e661f16',
+//                 poster_path: req.query.poster_path,
+//             }
+//         })
+//         return res.status(200).json(response.data);
+//     }
 
-
+//     catch(err) {
+//         console.log(err.message);
+//     }
+// })
 
 module.exports = router;
