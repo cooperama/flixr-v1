5ff53fd1d8e9c43bdfb6db418551113729eb415f
andyvdoan <andyy.doan@gmail.com>
2020-10-14 12:52:59 -0500

    Completed Dashboard template and included respective styles

--- public/js/app.js
+++ public/js/app.js
@@ -449,23 +449,23 @@ if (reviewBtn) {
 
 
 // -------------- Movie Recommendations Carousel
-$('.carousel .carousel-item').each(function(){
-  var minPerSlide = 3;
-  var next = $(this).next();
-  if (!next.length) {
-  next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
+// $('.carousel .carousel-item').each(function(){
+//   var minPerSlide = 3;
+//   var next = $(this).next();
+//   if (!next.length) {
+//   next = $(this).siblings(':first');
+//   }
+//   next.children(':first-child').clone().appendTo($(this));
   
-  for (var i=0;i<minPerSlide;i++) {
-      next=next.next();
-      if (!next.length) {
-        next = $(this).siblings(':first');
-      }
+//   for (var i=0;i<minPerSlide;i++) {
+//       next=next.next();
+//       if (!next.length) {
+//         next = $(this).siblings(':first');
+//       }
       
-      next.children(':first-child').clone().appendTo($(this));
-    }
-});
+//       next.children(':first-child').clone().appendTo($(this));
+//     }
+// });
 
 if (movieCarousel) {
   const moviePlaylist = [];
